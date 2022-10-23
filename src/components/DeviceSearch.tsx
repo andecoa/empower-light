f6f94f53442a91bae275032acd93e0a1efcd6129
Angelo Aldecoa <angelokn14@gmail.com>
2022-10-22 20:51:16 -0700

    fix default select

--- src/components/DeviceSearch.tsx
+++ src/components/DeviceSearch.tsx
@@ -19,7 +19,7 @@ const DeviceSearch = () => {
       onChange={handleSelect}
       className="w-60 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
       disabled={isLoading}
-      defaultValue={selectedDevice || 'disabled'}
+      value={selectedDevice || 'disabled'}
     >
       <option disabled value="disabled">
         {isLoading
