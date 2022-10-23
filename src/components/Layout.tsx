2b93d9a6f53eef5d6397f799fbef4f2c0a2f8687
Angelo Aldecoa <angelokn14@gmail.com>
2022-10-22 18:34:29 -0700

    create layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,46 @@
-const Layout = ({ children }: LayoutProps) => {
+import Link from 'next/link'
+
+const navigation = [
+  {
+    name: 'Device Details',
+    route: '/device-details',
+  },
+  {
+    name: 'Donations',
+    route: '/donations',
+  },
+  {
+    name: 'Settings',
+    route: '/settings',
+  },
+]
+
+export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex justify-center">
-      <div>
+    <div className="flex flex-nowrap h-screen">
+      <div className="w-80 bg-blue-300 p-10 pb-20 flex flex-col justify-between">
         <div>
-          <h2>Welcome,</h2>
-          <span>Empower2Transform</span>
+          <div className="mb-4">
+            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="50" cy="50" r="50" fill="white" />
+            </svg>
+          </div>
+          <div>
+            <h2 className="text-4xl font-bold text-blue-500">Welcome,</h2>
+            <span className="text-xl text-blue-500">Empower2Transform</span>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-2">
+          {navigation.map((navigation) => (
+            <Link href={navigation.route} key={navigation.name}>
+              <a className="px-6 py-2 bg-indigo-800 rounded-md text-center text-white">
+                {navigation.name}
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
-      <div>{children}</div>
+      <div className="p-10">{children}</div>
     </div>
   )
 }
@@ -15,5 +48,3 @@ const Layout = ({ children }: LayoutProps) => {
 type LayoutProps = {
   children: React.ReactNode
 }
-
-export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,7 @@
 import type { NextPage } from 'next'
-import { useDevices } from '../common/queries/useDevices'
 
 const Home: NextPage = () => {
-  const { data, isLoading, isError } = useDevices()
-
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (isError) {
-    return <div>Error encountered. Please try again.</div>
-  }
-
-  return <div>{JSON.stringify(data)}</div>
+  return <div>Home</div>
 }
 
 export default Home
